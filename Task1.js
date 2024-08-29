4bff06ef370429f228c012f212b3f34715462750
Nagendra1432 <nagendramejari@gmail.com>
2024-08-29 23:47:11 +0530

    message

---
+++ Task1.js
@@ -0,0 +1,76 @@
+// let num=1243.746
+// console.log(num.toPrecision(2))
+
+
+// let x=1234.6586
+// console.log(x.toFixed(3))
+
+// let x=123.456
+// console.log(parseInt(x))
+
+// let x=1234.9868325
+// console.log(parseFloat(x));
+
+
+
+// let num=[12,3.4,55,44,5,6,7.8]
+// let arr1=[]
+// let arr2=[]
+// for(i of num){
+//     if(Number.isInteger(i)){
+//         arr1.push(i)
+//     }
+//     else{
+//        arr2.push(i) 
+//     }
+// }
+// console.log(arr1.join())
+// console.log(arr2.join())
+
+
+// let arr1=["hello",2,4,"yellow","red"]
+// let nan=[]
+// let str=[]
+// for(i of arr1){
+//     if(isNaN(i)){
+//         nan.push(i)
+//     }
+//     else{
+//         str.push(i)
+//     }
+// }
+// console.log(nan)
+// console.log(str)\
+
+// let str1=12.344
+// console.log(Math.ceil(str1));
+
+// let str1=12.344
+// console.log(Math.floor(str1));
+
+// let atr1=(1,2,3,34)
+// console.log(Math.max(atr1));
+
+// let srt1=(1,2,3,4,5)
+// console.log(Math.min(srt1));
+
+// let arr1=12.456
+// console.log(Math.round(arr1));
+
+// let arr1=12.445
+// console.log(Math.round(arr1))
+
+// let arr1=[1,2,3,455,66]
+// let result=Math.max(...arr1)
+// console.log(...arr1);
+// console.log(result);
+
+
+// let arr1=(2,3)
+// console.log(Math.pow(2,3))
+
+// let arr1=[125,81,16,25]
+// let result=arr1.map(a=>Math.sqrt(a))
+// console.log(result)
+
+
